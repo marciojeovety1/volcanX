a66d1ab335f8711f9285c48e92a910eff88bcfcc
Marcio122 <marcio122@outlook.com.br>
2025-06-23 19:55:07 +0100

    just updating

--- src/services/VRFRewardDistributor.sol
+++ src/services/VRFRewardDistributor.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBase.sol";
 
 interface IOptionVaultForVRF {
     function lpBalances(address) external view returns (uint256);
